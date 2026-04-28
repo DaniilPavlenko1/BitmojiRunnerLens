82d3caa8b5beb6c753862f34e2b5f27f1d21600d
Daniil Pavlenko <daniil.pavlenko.2001@gmail.com>
2026-04-28 20:24:22 +0700

    Add obstacle movement and random lane respawn

---
+++ Assets/Scripts/Spawning/ObstacleMover.js
@@ -0,0 +1,51 @@
+// ObstacleMover.js
+
+//@input Component.ScriptComponent gameController
+//@input SceneObject[] obstacles
+//@input float moveSpeed = 700.0
+//@input float despawnZ = 1200.0
+//@input float respawnZ = -4800.0
+//@input float laneDistance = 150.0
+
+function getRandomLaneX() {
+    var lane = Math.floor(Math.random() * 3); // 0, 1, 2
+    return (lane - 1) * script.laneDistance;
+}
+
+function respawnObstacle(obstacle) {
+    var transform = obstacle.getTransform();
+    var pos = transform.getLocalPosition();
+
+    pos.x = getRandomLaneX();
+    pos.z = script.respawnZ - Math.random() * 1200;
+
+    transform.setLocalPosition(pos);
+}
+
+script.createEvent("UpdateEvent").bind(function () {
+    if (!script.gameController || !script.gameController.isRunning()) {
+        return;
+    }
+
+    var dt = getDeltaTime();
+
+    for (var i = 0; i < script.obstacles.length; i++) {
+        var obstacle = script.obstacles[i];
+
+        if (!obstacle) {
+            continue;
+        }
+
+        var transform = obstacle.getTransform();
+        var pos = transform.getLocalPosition();
+
+        pos.z += script.moveSpeed * dt;
+
+        if (pos.z > script.despawnZ) {
+            pos.x = getRandomLaneX();
+            pos.z = script.respawnZ - Math.random() * 1200;
+        }
+
+        transform.setLocalPosition(pos);
+    }
+});
