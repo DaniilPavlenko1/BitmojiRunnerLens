0bc39accc3df7aa8295efc7b7d49d046e5d2ac94
Daniil Pavlenko <daniil.pavlenko.2001@gmail.com>
2026-04-28 19:49:27 +0700

    Add moving world and endless ground loop

---
+++ Assets/Scripts/Spawning/MovingWorld.js
@@ -0,0 +1,34 @@
+// MovingWorld.js
+
+//@input Component.ScriptComponent gameController
+//@input SceneObject[] movingObjects
+//@input float moveSpeed = 250.0
+//@input float resetZ = -700.0
+//@input float segmentLength = 600.0
+
+script.createEvent("UpdateEvent").bind(function () {
+    if (!script.gameController || !script.gameController.isRunning()) {
+        return;
+    }
+
+    var dt = getDeltaTime();
+
+    for (var i = 0; i < script.movingObjects.length; i++) {
+        var obj = script.movingObjects[i];
+
+        if (!obj) {
+            continue;
+        }
+
+        var transform = obj.getTransform();
+        var pos = transform.getLocalPosition();
+
+        pos.z += script.moveSpeed * dt;
+
+        if (pos.z > script.resetZ) {
+            pos.z -= script.segmentLength * script.movingObjects.length;
+        }
+
+        transform.setLocalPosition(pos);
+    }
+});
